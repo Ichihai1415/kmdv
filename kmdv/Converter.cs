307f092de38727a7f218a4eae518c0632d86adb1
Ichihai1415 <projects3141.gif@gmail.com>
2023-09-22 13:41:03 +0900

    プロジェクト ファイルを追加します。

---
+++ kmdv/Converter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kmdv
+{
+    internal class Converter
+    {
+        /// <summary>
+        /// ColorからHSVを求めます。
+        /// </summary>
+        /// <remarks>参考:https://sourcechord.hatenablog.com/entry/20130710/1373476676</remarks>
+        /// <param name="color">元の色</param>
+        /// <returns>HSV</returns>
+        public static float[] Color2HSV(Color color)
+        {
+            float r = color.R / 255f;
+            float g = color.G / 255f;
+            float b = color.B / 255f;
+            float[] list = new float[] { r, g, b };
+            float max = list.Max();
+            float min = list.Min();
+            float h, s, v;
+            if (max == min)
+                h = 0;
+            else if (max == r)
+                h = (60f * (g - b) / (max - min) + 360f) % 360f;
+            else if (max == g)
+                h = 60f * (b - r) / (max - min) + 120f;
+            else
+                h = 60f * (r - g) / (max - min) + 240f;
+            if (max == 0)
+                s = 0;
+            else
+                s = (max - min) / max;
+            v = max;
+            return new float[] { h, s, v };
+        }
+
+        /// <summary>
+        /// HSVから強震モニタカラースケール値を求めます。
+        /// </summary>
+        /// <remarks>参考:https://qiita.com/NoneType1/items/a4d2cf932e20b56ca444</remarks>
+        /// <param name="hsv">HSV</param>
+        /// <returns>強震モニタカラースケール値</returns>
+        public static double Color2KCS (float[] hsv)
+        {
+            double p = 0;
+            float h = hsv[0]/360f;
+            float s = hsv[1];
+            float v = hsv[2];
+            if (v > 0.1 && s > 0.75)
+            {
+                if (h > 0.1476)
+                    p = 280.31 * Math.Pow(h, 6) - 916.05 * Math.Pow(h, 5) + 1142.6 * Math.Pow(h, 4) - 709.95 * Math.Pow(h, 3) + 234.65 * Math.Pow(h, 2) - 40.27 * h + 3.2217;
+                if (h <= 0.1476 && h > 0.001)
+                    p = 151.4 * Math.Pow(h, 4) - 49.32 * Math.Pow(h, 3) + 6.753 * Math.Pow(h, 2) - 2.481 * h + 0.9033;
+                if (h <= 0.001)
+                    p = -0.005171 * Math.Pow(v, 2) - 0.3282 * v + 1.2236;
+            }
+            if (p < 0)
+                p = 0;
+            return p;
+        }
+
+        /// <summary>
+        /// 強震モニタカラースケール値からPGAを求めます。
+        /// </summary>
+        /// <param name="kcs"></param>
+        /// <returns></returns>
+        public static double KCS2PGA(double kcs)
+        {
+            return Math.Pow(10, 5 * kcs - 2);
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ kmdv/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace kmdv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            Gettimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(30, 30, 60);
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(352, 400);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(30, 30, 60);
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Location = new Point(352, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(176, 200);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(30, 30, 60);
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(352, 200);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(176, 200);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 60, 90);
+            ClientSize = new Size(800, 400);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "kmdv";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private System.Windows.Forms.Timer Gettimer;
+    }
+}
