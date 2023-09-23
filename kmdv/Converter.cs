67796471584af850d5062b748d5f9c79b409329c
Ichihai1415 <projects3141.gif@gmail.com>
2023-09-23 18:09:29 +0900

    長周期・PGDを追加　その他調整・追加

--- kmdv/Converter.cs
+++ kmdv/Converter.cs
@@ -2,8 +2,132 @@
 
 namespace kmdv
 {
-    internal class Converter
+    public class Converter
     {
+        /// <summary>
+        /// 強震モニタ画像の種類
+        /// </summary>
+        /// <remarks>Sur:地表 Bor:地中 _lmoni:長周期サーバー</remarks>
+        public enum KyoshinImage
+        {
+            Sindo_Sur = 1,
+            Sindo_Bor = 2,
+            PGA_Sur = 3,
+            PGA_Bor = 4,
+            PGV_Sur = 5,
+            PGV_Bor = 6,
+            PGD_Sur = 7,
+            PGD_Bor = 8,
+            Res_0125_Sur = 9,
+            Res_0125_Bor = 10,
+            Res_0250_Sur = 11,
+            Res_0250_Bor = 12,
+            Res_0500_Sur = 13,
+            Res_0500_Bor = 14,
+            Res_1000_Sur = 15,
+            Res_1000_Bor = 16,
+            Res_2000_Sur = 17,
+            Res_2000_Bor = 18,
+            Res_4000_Sur = 19,
+            Res_4000_Bor = 20,
+            EEWSindo = 21,
+            PSWave = 22,
+
+            Sindo_Sur_lmoni = 31,
+            Sindo_Bor_lmoni = 32,
+            PGA_Sur_lmoni = 33,
+            PGA_Bor_lmoni = 34,
+            PGV_Sur_lmoni = 35,
+            PGV_Bor_lmoni = 36,
+            PGD_Sur_lmoni = 37,
+            PGD_Bor_lmoni = 38,
+            Res_0125_Sur_lmoni = 39,
+            Res_0125_Bor_lmoni = 40,
+            Res_0250_Sur_lmoni = 41,
+            Res_0250_Bor_lmoni = 42,
+            Res_0500_Sur_lmoni = 43,
+            Res_0500_Bor_lmoni = 44,
+            Res_1000_Sur_lmoni = 45,
+            Res_1000_Bor_lmoni = 46,
+            Res_2000_Sur_lmoni = 47,
+            Res_2000_Bor_lmoni = 48,
+            Res_4000_Sur_lmoni = 49,
+            Res_4000_Bor_lmoni = 50,
+            EEWSindo_lmoni = 51,
+            PSWave_lmoni = 52,
+
+            Cho_Kai = 61,
+            Cho_1s = 62,
+            Cho_2s = 63,
+            Cho_3s = 64,
+            Cho_4s = 65,
+            Cho_5s = 66,
+            Cho_6s = 67,
+            Cho_7s = 68
+        }
+
+        /// <summary>
+        /// 強震モニタ画像のURL
+        /// </summary>
+        /// <remarks>{time}:yyyyMMdd/yyyyMMddHHmmss</remarks>
+        public static readonly Dictionary<KyoshinImage, string> KyoshinURL = new()
+        {
+            { KyoshinImage.Sindo_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/jma_s/{time}.jma_s.gif" },
+            { KyoshinImage.Sindo_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/jma_b/{time}.jma_b.gif" },
+            { KyoshinImage.PGA_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/acmap_s/{time}.acmap_s.gif" },
+            { KyoshinImage.PGA_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/acmap_b/{time}.acmap_b.gif" },
+            { KyoshinImage.PGV_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/vcmap_s/{time}.vcmap_s.gif" },
+            { KyoshinImage.PGV_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/vcmap_b/{time}.vcmap_b.gif" },
+            { KyoshinImage.PGD_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/dcmap_s/{time}.dcmap_s.gif" },
+            { KyoshinImage.PGD_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/dcmap_b/{time}.dcmap_b.gif" },
+            { KyoshinImage.Res_0125_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp0125_s/{time}.rsp0125_s.gif" },
+            { KyoshinImage.Res_0125_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp0125_b/{time}.rsp0125_b.gif" },
+            { KyoshinImage.Res_0250_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp0250_s/{time}.rsp0250_s.gif" },
+            { KyoshinImage.Res_0250_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp0250_b/{time}.rsp0250_b.gif" },
+            { KyoshinImage.Res_0500_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp0500_s/{time}.rsp0500_s.gif" },
+            { KyoshinImage.Res_0500_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp0500_b/{time}.rsp0500_b.gif" },
+            { KyoshinImage.Res_1000_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp1000_s/{time}.rsp1000_s.gif" },
+            { KyoshinImage.Res_1000_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp1000_b/{time}.rsp1000_b.gif" },
+            { KyoshinImage.Res_2000_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp2000_s/{time}.rsp2000_s.gif" },
+            { KyoshinImage.Res_2000_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp2000_b/{time}.rsp2000_b.gif" },
+            { KyoshinImage.Res_4000_Sur, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp4000_s/{time}.rsp4000_s.gif" },
+            { KyoshinImage.Res_4000_Bor, "http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/rsp4000_b/{time}.rsp4000_b.gif" },
+            { KyoshinImage.EEWSindo, "http://www.kmoni.bosai.go.jp/data/map_img/EstShindoImg/eew/{time}.eew.gif" },
+            { KyoshinImage.PSWave, "http://www.kmoni.bosai.go.jp/data/map_img/PSWaveImg/eew/{time}.eew.gif" },
+
+            { KyoshinImage.Sindo_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/jma_s/{time}.jma_s.gif" },
+            { KyoshinImage.Sindo_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/jma_b/{time}.jma_b.gif" },
+            { KyoshinImage.PGA_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/acmap_s/{time}.acmap_s.gif" },
+            { KyoshinImage.PGA_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/acmap_b/{time}.acmap_b.gif" },
+            { KyoshinImage.PGV_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/vcmap_s/{time}.vcmap_s.gif" },
+            { KyoshinImage.PGV_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/vcmap_b/{time}.vcmap_b.gif" },
+            { KyoshinImage.PGD_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/dcmap_s/{time}.dcmap_s.gif" },
+            { KyoshinImage.PGD_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/dcmap_b/{time}.dcmap_b.gif" },
+            { KyoshinImage.Res_0125_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp0125_s/{time}.rsp0125_s.gif" },
+            { KyoshinImage.Res_0125_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp0125_b/{time}.rsp0125_b.gif" },
+            { KyoshinImage.Res_0250_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp0250_s/{time}.rsp0250_s.gif" },
+            { KyoshinImage.Res_0250_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp0250_b/{time}.rsp0250_b.gif" },
+            { KyoshinImage.Res_0500_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp0500_s/{time}.rsp0500_s.gif" },
+            { KyoshinImage.Res_0500_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp0500_b/{time}.rsp0500_b.gif" },
+            { KyoshinImage.Res_1000_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp1000_s/{time}.rsp1000_s.gif" },
+            { KyoshinImage.Res_1000_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp1000_b/{time}.rsp1000_b.gif" },
+            { KyoshinImage.Res_2000_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp2000_s/{time}.rsp2000_s.gif" },
+            { KyoshinImage.Res_2000_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp2000_b/{time}.rsp2000_b.gif" },
+            { KyoshinImage.Res_4000_Sur_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp4000_s/{time}.rsp4000_s.gif" },
+            { KyoshinImage.Res_4000_Bor_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/RealTimeImg/rsp4000_b/{time}.rsp4000_b.gif" },
+            { KyoshinImage.EEWSindo_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/EstShindoImg/eew/{time}.eew.gif" },
+            { KyoshinImage.PSWave_lmoni, "https://smi.lmoniexp.bosai.go.jp/data/map_img/PSWaveImg/eew/{time}.eew.gif" },
+
+            { KyoshinImage.Cho_Kai, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrspmx_s/{time}.abrspmx_s.gif" },
+            { KyoshinImage.Cho_1s, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrsp1s_s/{time}.abrsp1s_s.gif" },
+            { KyoshinImage.Cho_2s, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrsp2s_s/{time}.abrsp2s_s.gif" },
+            { KyoshinImage.Cho_3s, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrsp3s_s/{time}.abrsp3s_s.gif" },
+            { KyoshinImage.Cho_4s, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrsp4s_s/{time}.abrsp4s_s.gif" },
+            { KyoshinImage.Cho_5s, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrsp5s_s/{time}.abrsp5s_s.gif" },
+            { KyoshinImage.Cho_6s, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrsp6s_s/{time}.abrsp6s_s.gif" },
+            { KyoshinImage.Cho_7s, "https://www.lmoni.bosai.go.jp/monitor/data/data/map_img/RealTimeImg/abrsp7s_s/{time}.abrsp7s_s.gif" },
+        };
+
         /// <summary>
         /// 強震モニタ色からHSVを求めます。
         /// </summary>
@@ -105,13 +229,21 @@ namespace kmdv
         /// 強震モニタカラースケール値からPGAを求めます。
         /// </summary>
         /// <param name="kcs"></param>
-        /// <returns></returns>
+        /// <returns>PGA</returns>
         public static double KCS2PGA(double kcs)
         {
             return Math.Pow(10, 5 * kcs - 2);
         }
 
-
+        /// <summary>
+        /// 強震モニタカラースケール値から強震震度を求めます。
+        /// </summary>
+        /// <param name="kcs">強震モニタカラースケール値</param>
+        /// <returns>強震震度</returns>
+        public static double KCS2Sindo(double kcs)
+        {
+            return 10 * kcs - 3;
+        }
 
 
 
@@ -119,7 +251,7 @@ namespace kmdv
     }
     class ArrayEqualityComparer<T> : IEqualityComparer<T[]>
     {
-        public bool Equals(T[] x, T[] y)
+        public bool Equals(T[]? x, T[]? y)
         {
             return StructuralComparisons.StructuralEqualityComparer.Equals(x, y);
         }
--- kmdv/Form1.cs
+++ kmdv/Form1.cs
@@ -12,7 +12,6 @@ namespace kmdv
         }
 
         public static Dictionary<int[], double> RGB2kcs = new(new ArrayEqualityComparer<int>())
-
         {
             { new int[]{ 0, 0, 0 }, 0 }
         };
@@ -29,36 +28,49 @@ namespace kmdv
         {
             Gettimer.Interval = 1000 + getDelay % 1000 - DateTime.Now.Millisecond % 1000;
             DateTime getTime = DateTime.Now.AddSeconds(-1);
-            Task<Bitmap> PGATask = Task.Run(() => Get_PGA(getTime));
 
-
-            await Task.WhenAll(PGATask);
+            Task<Bitmap> PGATask = Task.Run(() => GetKyoshin(KyoshinImage.PGA_Sur, getTime));
+            Task<Bitmap> SindoTask = Task.Run(() => GetKyoshin(KyoshinImage.Sindo_Sur, getTime));
+            Task<Bitmap> ChoTask = Task.Run(() => GetKyoshin(KyoshinImage.Cho_Kai, getTime));
+            Task<Bitmap> PGDTask = Task.Run(() => GetKyoshin(KyoshinImage.PGD_Sur, getTime));
+            await Task.WhenAll(PGATask,SindoTask,ChoTask,PGDTask);
             Bitmap PGAImg = PGATask.Result;
-
-            Task<double[]> PGAkcs = Task.Run(() => Image2kcs(PGAImg));
-
-            await Task.WhenAll(PGAkcs);
-
+            Bitmap SindoImg = SindoTask.Result;
+            Bitmap ChoImg = ChoTask.Result;
+            Bitmap PGDImg = PGDTask.Result;
+            Task<double[]> PGAkcsTask = Task.Run(() => Image2kcs(PGAImg));
+            Task<double[]> SindokcsTask = Task.Run(() => Image2kcs(SindoImg));
+            await Task.WhenAll(PGAkcsTask,SindokcsTask);
             Bitmap mainImg = new(528, 400);
             Graphics g = Graphics.FromImage(mainImg);
+            if(PGAImg.Width==352)
             g.DrawImage(PGAImg, 0, 0);
-            g.DrawImage(PGAImg, 352, 0,176,200);
-            g.DrawImage(PGAImg, 352, 200, 176, 200);
+            if (ChoImg.Width == 352)
+                g.DrawImage(ChoImg, 352, 0, 176, 200);
+            if(PGDImg.Width==352)
+            g.DrawImage(PGDImg, 352, 200, 176, 200);
 
-
-            double PGAkcsSum = PGAkcs.Result[0];
-            double PGAkcsMax = PGAkcs.Result[1];
+            double PGAkcsSum = PGAkcsTask.Result[0];
+            double PGAkcsMax = PGAkcsTask.Result[1];
             Debug.WriteLine($"{PGAkcsSum}  {PGAkcsMax} ({KCS2PGA(PGAkcsMax)})");
+            double SindokcsSum = SindokcsTask.Result[0];
+            double SindokcsMax = SindokcsTask.Result[1];
+            Debug.WriteLine($"{SindokcsSum}  {SindokcsMax} ({KCS2Sindo(SindokcsMax)})");
             MainImage.Image = mainImg;
             g.Dispose();
 
         }
 
-        public static async Task<Bitmap> Get_PGA(DateTime time)
+
+        /// <summary>
+        /// 強震モニタの画像を取得します。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="time"></param>
+        /// <returns>強震モニタの画像。失敗時はBitmap(0, 0)。</returns>
+        public static async Task<Bitmap> GetKyoshin(KyoshinImage type, DateTime time)
         {
-            Debug.WriteLine($"get_pga");
-            //http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/acmap_s/20230922/20230922203835.acmap_s.gif
-            HttpResponseMessage res = await client.GetAsync($"http://www.kmoni.bosai.go.jp/data/map_img/RealTimeImg/acmap_s/{time:yyyyMMdd/yyyyMMddHHmmss}.acmap_s.gif");
+            HttpResponseMessage res = await client.GetAsync(KyoshinURL[type].Replace("{time}", time.ToString("yyyyMMdd/yyyyMMddHHmmss")));
             if (res.StatusCode != HttpStatusCode.OK)
                 return new Bitmap(0, 0);
             return new Bitmap(res.Content.ReadAsStream());
@@ -68,6 +80,8 @@ namespace kmdv
         public static double[] Image2kcs(Bitmap image)
         {
             Debug.WriteLine($"image2kcs");
+            if (image.Width != 352 || image.Height != 400)
+                return new double[] { 0, 0 };
             double kcsSum = 0;
             double kcsMax = 0;
             for (int i = 0; i < 352; i++)
@@ -87,7 +101,6 @@ namespace kmdv
                     }
                     kcsMax = Math.Max(kcsMax, kcs);
                     kcsSum += kcs;
-                    //throw new Exception();
                 }
             return new double[] { kcsSum, kcsMax };
         }
