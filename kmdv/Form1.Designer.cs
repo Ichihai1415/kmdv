526040a4ac4f29d246b756368384aa3ee815ffbb
Ichihai1415 <projects3141.gif@gmail.com>
2023-09-27 00:54:28 +0900

    グラフの追加　表示の調整

--- kmdv/Form1.Designer.cs
+++ kmdv/Form1.Designer.cs
@@ -33,14 +33,16 @@
             MainImage = new PictureBox();
             Text1 = new Label();
             groupBox1 = new GroupBox();
-            VersionView = new Label();
+            KCSGraph = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
+            MaxsView = new TextBox();
+            KCSView_rss = new Label();
             Text0 = new Label();
-            KCSview = new Label();
+            KCSView_acs = new Label();
+            VersionView = new Label();
             Text2 = new Label();
             RAMview = new Label();
             LogView = new TextBox();
             MSView = new Label();
-            MaxsView = new TextBox();
             ((System.ComponentModel.ISupportInitialize)MainImage).BeginInit();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -62,71 +64,107 @@
             // Text1
             // 
             Text1.AutoSize = true;
-            Text1.Location = new Point(6, 32);
+            Text1.Location = new Point(6, 25);
             Text1.Name = "Text1";
-            Text1.Size = new Size(103, 90);
+            Text1.Size = new Size(127, 30);
             Text1.TabIndex = 4;
-            Text1.Text = "rs-s max:\r\nac-s max:\r\nac-s sum:";
+            Text1.Text = "rss:        acs:";
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(VersionView);
+            groupBox1.Controls.Add(KCSGraph);
+            groupBox1.Controls.Add(MaxsView);
+            groupBox1.Controls.Add(KCSView_rss);
             groupBox1.Controls.Add(Text0);
             groupBox1.Controls.Add(Text1);
-            groupBox1.Controls.Add(KCSview);
-            groupBox1.Location = new Point(534, 0);
+            groupBox1.Controls.Add(KCSView_acs);
+            groupBox1.Location = new Point(534, 35);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(260, 131);
+            groupBox1.Size = new Size(260, 254);
             groupBox1.TabIndex = 5;
             groupBox1.TabStop = false;
             groupBox1.Text = "KCS";
+            // 
+            // KCSGraph
+            // 
+            KCSGraph.Location = new Point(5, 58);
+            KCSGraph.Margin = new Padding(3, 4, 3, 4);
+            KCSGraph.Name = "KCSGraph";
+            KCSGraph.Size = new Size(245, 84);
+            KCSGraph.TabIndex = 11;
+            // 
+            // MaxsView
+            // 
+            MaxsView.AcceptsReturn = true;
+            MaxsView.BackColor = Color.FromArgb(30, 60, 90);
+            MaxsView.BorderStyle = BorderStyle.FixedSingle;
+            MaxsView.Font = new Font("Yu Gothic UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            MaxsView.ForeColor = Color.White;
+            MaxsView.Location = new Point(6, 148);
+            MaxsView.Multiline = true;
+            MaxsView.Name = "MaxsView";
+            MaxsView.ReadOnly = true;
+            MaxsView.ScrollBars = ScrollBars.Vertical;
+            MaxsView.Size = new Size(248, 100);
+            MaxsView.TabIndex = 10;
+            MaxsView.TabStop = false;
+            // 
+            // KCSView_rss
+            // 
+            KCSView_rss.Location = new Point(42, 25);
+            KCSView_rss.Name = "KCSView_rss";
+            KCSView_rss.Size = new Size(49, 30);
+            KCSView_rss.TabIndex = 10;
+            KCSView_rss.Text = "----";
+            KCSView_rss.TextAlign = ContentAlignment.TopRight;
+            // 
+            // Text0
+            // 
+            Text0.AutoSize = true;
+            Text0.Font = new Font("Yu Gothic UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            Text0.Location = new Point(9, 2);
+            Text0.Name = "Text0";
+            Text0.Size = new Size(71, 25);
+            Text0.TabIndex = 9;
+            Text0.Text = "<KCS>";
+            // 
+            // KCSView_acs
+            // 
+            KCSView_acs.Location = new Point(9, 25);
+            KCSView_acs.Name = "KCSView_acs";
+            KCSView_acs.Size = new Size(242, 30);
+            KCSView_acs.TabIndex = 5;
+            KCSView_acs.Text = "----/----";
+            KCSView_acs.TextAlign = ContentAlignment.TopRight;
             // 
             // VersionView
             // 
             VersionView.AutoSize = true;
             VersionView.Font = new Font("Yu Gothic UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
-            VersionView.Location = new Point(200, 0);
+            VersionView.Location = new Point(738, 0);
             VersionView.Name = "VersionView";
             VersionView.Size = new Size(62, 13);
             VersionView.TabIndex = 9;
             VersionView.Text = "kmdv v0.0.0";
             // 
-            // Text0
-            // 
-            Text0.AutoSize = true;
-            Text0.Location = new Point(9, 2);
-            Text0.Name = "Text0";
-            Text0.Size = new Size(82, 30);
-            Text0.TabIndex = 9;
-            Text0.Text = "<KCS>";
-            // 
-            // KCSview
-            // 
-            KCSview.Location = new Point(6, 32);
-            KCSview.Name = "KCSview";
-            KCSview.Size = new Size(242, 90);
-            KCSview.TabIndex = 5;
-            KCSview.Text = " - - - - \r\n - - - - \r\n - - - - ";
-            KCSview.TextAlign = ContentAlignment.TopRight;
-            // 
             // Text2
             // 
             Text2.AutoSize = true;
             Text2.Font = new Font("Yu Gothic UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            Text2.Location = new Point(528, 134);
+            Text2.Location = new Point(528, 10);
             Text2.Name = "Text2";
-            Text2.Size = new Size(185, 25);
+            Text2.Size = new Size(193, 25);
             Text2.TabIndex = 6;
-            Text2.Text = "ram:                   time:";
+            Text2.Text = "ram:             MB  time:";
             // 
             // RAMview
             // 
             RAMview.Font = new Font("Yu Gothic UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            RAMview.Location = new Point(570, 134);
+            RAMview.Location = new Point(571, 10);
             RAMview.Name = "RAMview";
-            RAMview.Size = new Size(95, 25);
+            RAMview.Size = new Size(66, 25);
             RAMview.TabIndex = 7;
-            RAMview.Text = "- - - - MB";
+            RAMview.Text = "--------";
             RAMview.TextAlign = ContentAlignment.TopRight;
             // 
             // LogView
@@ -136,47 +174,31 @@
             LogView.BorderStyle = BorderStyle.FixedSingle;
             LogView.Font = new Font("Yu Gothic UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
             LogView.ForeColor = Color.White;
-            LogView.Location = new Point(534, 281);
+            LogView.Location = new Point(534, 294);
             LogView.Multiline = true;
             LogView.Name = "LogView";
             LogView.ReadOnly = true;
             LogView.ScrollBars = ScrollBars.Vertical;
-            LogView.Size = new Size(260, 112);
+            LogView.Size = new Size(260, 100);
             LogView.TabIndex = 8;
             LogView.TabStop = false;
             // 
             // MSView
             // 
             MSView.Font = new Font("Yu Gothic UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            MSView.Location = new Point(662, 134);
+            MSView.Location = new Point(662, 10);
             MSView.Name = "MSView";
             MSView.Size = new Size(138, 25);
             MSView.TabIndex = 9;
-            MSView.Text = " - - - - ms";
+            MSView.Text = "------ms";
             MSView.TextAlign = ContentAlignment.TopRight;
-            // 
-            // MaxsView
-            // 
-            MaxsView.AcceptsReturn = true;
-            MaxsView.BackColor = Color.FromArgb(30, 60, 90);
-            MaxsView.BorderStyle = BorderStyle.FixedSingle;
-            MaxsView.Font = new Font("Yu Gothic UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            MaxsView.ForeColor = Color.White;
-            MaxsView.Location = new Point(534, 163);
-            MaxsView.Multiline = true;
-            MaxsView.Name = "MaxsView";
-            MaxsView.ReadOnly = true;
-            MaxsView.ScrollBars = ScrollBars.Vertical;
-            MaxsView.Size = new Size(260, 112);
-            MaxsView.TabIndex = 10;
-            MaxsView.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleMode = AutoScaleMode.None;
             BackColor = Color.FromArgb(30, 60, 90);
             ClientSize = new Size(800, 400);
-            Controls.Add(MaxsView);
+            Controls.Add(VersionView);
             Controls.Add(RAMview);
             Controls.Add(Text2);
             Controls.Add(MSView);
@@ -202,7 +224,7 @@
         private PictureBox MainImage;
         private Label Text1;
         private GroupBox groupBox1;
-        private Label KCSview;
+        private Label KCSView_acs;
         private Label Text2;
         private Label RAMview;
         private TextBox LogView;
@@ -210,5 +232,7 @@
         private Label VersionView;
         private Label MSView;
         private TextBox MaxsView;
+        private Label KCSView_rss;
+        private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart KCSGraph;
     }
 }
--- kmdv/Form1.cs
+++ kmdv/Form1.cs
@@ -1,3 +1,10 @@
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net;
 using static kmdv.Converter;
@@ -17,19 +24,126 @@ namespace kmdv
         };
         public readonly static HttpClient client = new();
         public readonly static int getDelay = 1500;
-        public static double[] kcsMaxs = new double[] { 9, 0, 0, 0 };//最後の分/10(切り捨てt),rssm,acsm,acss
+        public static double[] kcsMaxs = new double[] { 9, 0, 0, 0 };//最後の分/10(切り捨て),rssm,acsm,acss
         public static string kcsMaxsText = "";
+        public static ObservableCollection<ObservableValue> GraphValue_acss = new()
+        {
+            new ObservableValue(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0),
+             new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0),
+             new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0)
+        };
+        public static ObservableCollection<ObservableValue> GraphValue_acsm = new()
+        {
+            new ObservableValue(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0),
+             new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0),
+             new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0)
+        };
+        public static ObservableCollection<ObservableValue> GraphValue_rssm = new()
+        {
+            new ObservableValue(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0),
+             new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0),
+             new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0), new(0)
+        };
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            VersionView.Text = "kmdv v0.2.4";
+            VersionView.Text = "kmdv v0.3.0";
             LogView.Text = $"start:{DateTime.Now:yyyy/MM/dd HH:mm:ss}";
             Gettimer.Interval = 1000 + getDelay % 1000 - DateTime.Now.Millisecond % 1000;
             Gettimer.Enabled = true;
+
+            //setup
+            KCSGraph.DrawMargin = new Margin(0);
+            KCSGraph.Tooltip = null;
+            KCSGraph.XAxes = new Axis[]
+            {
+                new Axis
+                {
+                    Name="time(30s)",
+                    NamePaint = new SolidColorPaint(SKColors.White),
+                    NameTextSize = 12,
+
+                    LabelsPaint = new SolidColorPaint(SKColors.White),
+                    TextSize = 10,
+
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 1 }
+                }
+            };
+            KCSGraph.YAxes = new Axis[]
+            {
+                new Axis
+                {
+                    Name="kcs (ac-s sum)",
+                    NamePaint = new SolidColorPaint(SKColors.Red),
+                    NameTextSize = 12,
+
+                    LabelsPaint = new SolidColorPaint(SKColors.Red),
+                    TextSize = 12,
+
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 1 }
+                },
+                new Axis
+                {
+                    Name="kcs (ac-s max)",
+                    NamePaint = new SolidColorPaint(SKColors.Yellow),
+                    NameTextSize = 10,
+
+                    LabelsPaint = new SolidColorPaint(SKColors.Yellow),
+                    TextSize = 10,
+
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 1 }
+                },
+                new Axis
+                {
+                    Name="kcs (rs-s max)",
+                    NamePaint = new SolidColorPaint(SKColors.LightGreen),
+                    NameTextSize = 10,
+
+                    LabelsPaint = new SolidColorPaint(SKColors.LightGreen),
+                    TextSize = 10,
+
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 1 },
+                    Position = AxisPosition.End
+                }
+            };
+            KCSGraph.Series = new ObservableCollection<ISeries>
+            {
+                new LineSeries<ObservableValue?>
+                {
+
+                    Values = GraphValue_acss,
+                    Stroke = new SolidColorPaint(SKColors.Red, 1),
+                    GeometrySize = 0,
+                    Fill = null,
+                    GeometryStroke = null,
+                    ScalesYAt = 0
+                },
+                new LineSeries<ObservableValue?>
+                {
+
+                    Values = GraphValue_acsm,
+                    Stroke = new SolidColorPaint(SKColors.Yellow, 1),
+                    GeometrySize = 0,
+                    Fill = null,
+                    GeometryStroke = null,
+                    ScalesYAt = 1
+                },
+                new LineSeries<ObservableValue?>
+                {
+
+                    Values = GraphValue_rssm,
+                    Stroke = new SolidColorPaint(SKColors.LightGreen, 1),
+                    GeometrySize = 0,
+                    Fill = null,
+                    GeometryStroke = null,
+                    ScalesYAt = 2
+                }
+            };
         }
 
         private async void Gettimer_Tick(object sender, EventArgs e)
         {
+
             Gettimer.Interval = 1000 + getDelay % 1000 - DateTime.Now.Millisecond % 1000;
             DateTime getTime = DateTime.Now.AddSeconds(-1);
 
@@ -45,7 +159,7 @@ namespace kmdv
             Bitmap PGDImg = PGDTask.Result;
 
             Task<double[]> PGAkcsTask = Task.Run(() => Image2kcs(PGAImg));
-            Task<double[]> SindokcsTask = Task.Run(() => Image2kcs(SindoImg));
+            Task<double> SindokcsTask = Task.Run(() => Image2kcsSindo(SindoImg));
             await Task.WhenAll(PGAkcsTask, SindokcsTask);
 
             Bitmap mainImg = new(528, 400);
@@ -59,18 +173,18 @@ namespace kmdv
 
             double PGAkcsSum = PGAkcsTask.Result[0];
             double PGAkcsMax = PGAkcsTask.Result[1];
-            Debug.WriteLine($"{PGAkcsSum}  {PGAkcsMax} ({KCS2PGA(PGAkcsMax)})");
-            double SindokcsSum = SindokcsTask.Result[0];
-            double SindokcsMax = SindokcsTask.Result[1];
-            Debug.WriteLine($"{SindokcsSum}  {SindokcsMax} ({KCS2Sindo(SindokcsMax)})");
+            double SindokcsMax = SindokcsTask.Result;
             MainImage.Image = mainImg;
             g.Dispose();
 
-            Debug.WriteLine($"{Environment.WorkingSet / 1048576}MB");
+            GraphInsert_acss(PGAkcsSum);
+            GraphInsert_acsm(PGAkcsMax);
+            GraphInsert_rssm(SindokcsMax);
 
-            KCSview.Text = $"{SindokcsMax:.0000}\n{PGAkcsMax:.0000}\n{PGAkcsSum:.0000}";
-            RAMview.Text = $"{Environment.WorkingSet / 1048576d:0.00}MB";
-            MSView.Text = $"{(DateTime.Now - getTime.AddSeconds(1)).TotalMilliseconds:0.000}ms";
+            KCSView_rss.Text = $"{SindokcsMax * 100:0}";
+            KCSView_acs.Text = $"{PGAkcsSum:0}/{PGAkcsMax:0.00}";
+            RAMview.Text = $"{Environment.WorkingSet / 1048576d:0.0}";
+            MSView.Text = $"{(DateTime.Now - getTime.AddSeconds(1)).TotalMilliseconds:0.0}ms";
 
             double min_10 = Math.Floor(getTime.Minute / 10d);
             if (min_10 == kcsMaxs[0])
@@ -88,8 +202,6 @@ namespace kmdv
                 kcsMaxs[3] = PGAkcsSum;
             }
             MaxsView.Text = $"{getTime:HH}:{kcsMaxs[0]}0~ rssm:{kcsMaxs[1]:.000} acsm:{kcsMaxs[2]:.000} acss:{kcsMaxs[3]:0}\r\n{kcsMaxsText}";
-
-
         }
 
 
@@ -105,7 +217,7 @@ namespace kmdv
             {
                 HttpResponseMessage res = await client.GetAsync(KyoshinURL[type].Replace("{time}", time.ToString("yyyyMMdd/yyyyMMddHHmmss")));
                 if (res.StatusCode != HttpStatusCode.OK)
-                    throw new Exception("取得失敗");
+                    throw new Exception($"取得失敗({res.StatusCode})");
                 return new Bitmap(res.Content.ReadAsStream());
             }
             catch (Exception ex)
@@ -156,6 +268,76 @@ namespace kmdv
                 LogView.Text = $"-----{DateTime.Now:MM/dd HH:mm:ss} <Image2kcs>\r\n{ex}\r\n{LogView.Text}";
                 return new double[] { 0, 0 };
             }
+        }
+
+        /// <summary>
+        /// 画像から強震モニタカラースケールの強震震度を取得します。
+        /// </summary>
+        /// <remarks>テーブル取得式です。カラースケール計算より正確と思われます。</remarks>
+        /// <param name="image">強震モニタの画像。</param>
+        /// <returns>kcsの最大</returns>
+        public double Image2kcsSindo(Bitmap image)
+        {
+            try
+            {
+                Debug.WriteLine($"image2kcs");
+                if (image.Width != 352 || image.Height != 400)
+                    return 0;
+                double kcsSum = 0;
+                double kcsMax = 0;
+                for (int i = 0; i < 352; i++)
+                    for (int j = 0; j < 400; j++)
+                    {
+                        Color color = image.GetPixel(i, j);
+                        int[] colors = new int[] { color.R, color.G, color.B };
+                        if (colors.Sum() == 0)
+                            continue;
+                        double kcs = (RGB2Sindo[colors] + 3d) / 10d;
+                        kcsMax = Math.Max(kcsMax, kcs);
+                        kcsSum += kcs;
+                    }
+                return kcsMax;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                LogView.Text = $"-----{DateTime.Now:MM/dd HH:mm:ss} <Image2kcsSindo>\r\n{ex}\r\n{LogView.Text}";
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// acssグラフ用データ配列に値を挿入します。
+        /// </summary>
+        /// <param name="value">挿入する値。0の場合nullとなります。</param>
+        public static void GraphInsert_acss(double value)
+        {
+            for (int i = 0; i < 29; i++)
+                GraphValue_acss[i] = GraphValue_acss[i + 1];
+            GraphValue_acss[29] = new ObservableValue(value == 0 ? null : value);
+        }
+
+        /// <summary>
+        /// acsmグラフ用データ配列に値を挿入します。
+        /// </summary>
+        /// <param name="value">挿入する値。0の場合nullとなります。</param>
+        public static void GraphInsert_acsm(double value)
+        {
+            for (int i = 0; i < 29; i++)
+                GraphValue_acsm[i] = GraphValue_acsm[i + 1];
+            GraphValue_acsm[29] = new ObservableValue(value == 0 ? null : value);
+        }
+
+        /// <summary>
+        /// rssmグラフ用データ配列に値を挿入します。
+        /// </summary>
+        /// <param name="value">挿入する値。0の場合nullとなります。</param>
+        public static void GraphInsert_rssm(double value)
+        {
+            for (int i = 0; i < 29; i++)
+                GraphValue_rssm[i] = GraphValue_rssm[i + 1];
+            GraphValue_rssm[29] = new ObservableValue(value == 0 ? null : value);
         }
     }
 }
