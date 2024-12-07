17d182431b460da56d6e997d5ab563e39589f32c
Ichihai1415 <projects3141.gif@gmail.com>
2024-12-07 22:22:10 +0900

    PGAkcsMaxによる音声再生タイミング修正

--- kmdv/Form1.cs
+++ kmdv/Form1.cs
@@ -325,20 +325,18 @@ namespace kmdv
                 }
                 else
                     KCSLevel_acss.BackColor = Color.White;
+            if (getTime.Second % 2 == 1)
+                if (PGAkcsMax >= 0.8)//0.8で100 0.6で10 0.66でだいたい20 (log10(x)+2)/5
+                    PlaySound("pga100+.wav", true);
+                else if (PGAkcsMax >= 0.66)
+                    PlaySound("pga20+.wav", true);
             if (PGAkcsMax >= 0.8)//0.8で100 0.6で10 0.66でだいたい20 (log10(x)+2)/5
-            {
-                PlaySound("pga100+.wav", true);
                 if (PGAkcsMax >= 0.9)
                     KCSLevel_acsm.BackColor = Color.FromArgb(200, 0, 200);
                 else
                     KCSLevel_acsm.BackColor = Color.Red;
-
-            }
             else if (PGAkcsMax >= 0.66)
-            {
-                PlaySound("pga20+.wav", true);
                 KCSLevel_acsm.BackColor = Color.Yellow;
-            }
             else
                 KCSLevel_acsm.BackColor = Color.White;
 
